d16a7032ffb11ac38db9cefca76319400f2ee679
1011karsw <56419811+1011karsw@users.noreply.github.com>
2019-10-28 11:28:48 +0900

    Add files via upload

--- main.ts
+++ main.ts
@@ -64,16 +64,6 @@ namespace TFabConnect {
         return receiveNumber;
     }
 
-    /**
-     * setting wait time.
-     * @param msec , eg:5000
-     */
-    //% blockId=set_wait_time block="応答待ち時間(ミリ秒）%msec"
-    //% advanced=true
-    export function setWaitTime(msec: number) {
-        waitTime = msec;
-    }
-
     function isNaN(x: number) {
         return x !== x;
     }
@@ -210,4 +200,3 @@ namespace TFabConnect {
         }
     }
 }
-
